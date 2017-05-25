d11136fa3177c08e3cbf9071a7759ea27ff22e29
elijahprince73 <elijahprince73@gmail.com>
2017-05-25 14:30:34 -0700

    added babel-preset-stage-0

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var obj1 = {
+  name: "Elijah",
+  location: "Texas"
+}
+
+var obj2 = {
+  age : 20,
+  ...obj1
+}
+
+console.log(obj2);
+
 ReactDOM.render(
   <h1>Boilerplate app!</h1>,
   document.getElementById('app')
