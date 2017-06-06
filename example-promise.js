abbc677e3eed462f04dde63d99a0527990294c2e
Elijah Prince <elijahprince73@gmail.com>
2017-06-05 19:15:29 -0700

    fixed facking call

--- example-promise.js
+++ example-promise.js
@@ -1,45 +1,50 @@
-// function getTempCallback(location, callback) {
-//   callback(undefined, 78)
-//   callback("City Not Found")
+// function getTempCallback (location, callback) {
+//   callback(undefined, 78);
+//   callback('City not found');
 // }
 //
-// getTempCallback("Texas", function (err, temp) {
-// if (err){
-//   console.log('error', err);
-// }
-// else {
-//   console.log('success', temp  );
-// }
-// })
-
+// getTempCallback('Philadelphia', function (err, temp) {
+//   if (err) {
+//     console.log('error', err);
+//   } else {
+//     console.log('success', temp)
+//   }
+// });
+//
 // function getTempPromise (location) {
-//   return new Promise (function (resolve, reject) {
+//   return new Promise(function (resolve, reject) {
 //     setTimeout(function () {
 //       resolve(79);
-//       reject('City Not Found');
-//     },1000)
-//   })
+//       reject('City not found');
+//     }, 1000);
+//   });
 // }
 //
-// getTempPromise('Texas').then(function (temp) {
-// console.log('Promise Success', temp);
+// getTempPromise('Philadelphia').then(function (temp) {
+//   console.log('promise success', temp);
 // }, function (err) {
-// console.log('Promise error', err);
-// })
+//   console.log('promise error', err);
+// });
 
-
-function addPromise(a, b) {
+// Challenge Area
+function addPromise (a, b) {
   return new Promise(function (resolve, reject) {
-    if (typeof a  == 'number' && typeof b  == 'number') {
+    if (typeof a === 'number' && typeof b === 'number') {
       resolve(a + b);
-    }else {
-      reject("A and B need to be numbers")
+    } else {
+      reject('A & b need to be numbers');
     }
-  })
+  });
 }
 
-addPromise("1", 2).then(function (addedNumber) {
-  console.log('Number', addedNumber);
+addPromise(2, 3).then(function (sum) {
+  console.log('success', sum);
 }, function (err) {
-  console.log("Error", err);
-})
+  console.log('error', err);
+});
+
+addPromise('andrew', 9).then(function (sum) {
+  console.log('this should not show up');
+}, function (err) {
+  console.log('This should appear', err);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -7,24 +7,26 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
-      Main: 'app/components/main.jsx',
+      Main: 'app/components/Main.jsx',
       Nav: 'app/components/Nav.jsx',
       Weather: 'app/components/Weather.jsx',
-      About:'app/components/About.jsx',
-      Example: 'app/components/Example.jsx',
-      WeatherForm:'app/components/WeatherForm.jsx',
-      WeatherMessage: 'app/components/WeatherMessage.jsx'
+      WeatherForm: 'app/components/WeatherForm.jsx',
+      WeatherMessage: 'app/components/WeatherMessage.jsx',
+      About: 'app/components/About.jsx',
+      Examples: 'app/components/Examples.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [{
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015', 'stage-0']
-      },
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/
-    }]
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
   }
 };
