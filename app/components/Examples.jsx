3e9ad320e4da4817b830f31284722ba4f71d8c92
Elijah Prince <elijahprince73@gmail.com>
2017-06-13 19:37:20 -0700

    centered text

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
+var {Link} =  require('react-router');
 
 var Examples = (props) => {
       return (
         <div>
-        <h3>Examples </h3>
+        <h1 className="text-center">Examples </h1>
+        <p>Here are a few example locations to try</p>
+        <ol>
+          <li>
+            <Link to='/?location=Austin'>Austin, TX</Link>
+          </li>
+          <li>
+            <Link to='/?location=rio'> Rio, Brazil</Link>
+          </li>
+        </ol>
       </div>
       )
     }
--- public/bundle.js
+++ public/bundle.js
@@ -25497,11 +25497,14 @@
 	    null,
 	    React.createElement(Nav, null),
 	    React.createElement(
-	      'h2',
-	      null,
-	      'Main Components'
-	    ),
-	    props.children
+	      'div',
+	      { className: 'row' },
+	      React.createElement(
+	        'div',
+	        { className: 'columns medium-6 large-4 small-centered' },
+	        props.children
+	      )
+	    )
 	  );
 	};
 
@@ -26888,14 +26891,44 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 	  return React.createElement(
 	    'div',
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
+	      'h1',
+	      { className: 'text-center' },
 	      'Examples '
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Here are a few example locations to try'
+	    ),
+	    React.createElement(
+	      'ol',
+	      null,
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Austin' },
+	          'Austin, TX'
+	        )
+	      ),
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=rio' },
+	          ' Rio, Brazil'
+	        )
+	      )
 	    )
 	  );
 	};
