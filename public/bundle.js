ecc306a8309e05efb3dcbdea6b6e0109f2b4928e
Elijah Prince <elijahprince73@gmail.com>
2017-06-13 20:09:55 -0700

    add styles to weather page

--- public/bundle.js
+++ public/bundle.js
@@ -25649,7 +25649,7 @@
 	      if (isLoading) {
 	        return React.createElement(
 	          'h3',
-	          null,
+	          { className: 'text-center' },
 	          ' Fetching Weather ... '
 	        );
 	      } else if (temp && location) {
@@ -25661,9 +25661,9 @@
 	      'div',
 	      null,
 	      React.createElement(
-	        'h3',
-	        null,
-	        'Weather Component'
+	        'h1',
+	        { className: 'text-center' },
+	        'Get Weather'
 	      ),
 	      React.createElement(WeatherForm, { onSearch: this.handleSearch }),
 	      renderMessage()
@@ -25704,7 +25704,7 @@
 	        React.createElement('input', { type: 'text', ref: 'location' }),
 	        React.createElement(
 	          'button',
-	          null,
+	          { className: 'button expanded hollow' },
 	          'Get Weather'
 	        )
 	      )
@@ -25718,7 +25718,7 @@
 /* 233 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
@@ -25727,13 +25727,13 @@
 	      location = _ref.location;
 
 	  return React.createElement(
-	    'h3',
-	    null,
-	    'It\'s it ',
+	    "h3",
+	    { className: "text-center" },
+	    "It's it ",
 	    temp,
-	    ' in ',
+	    " in ",
 	    location,
-	    '.'
+	    "."
 	  );
 	};
 	module.exports = WeatherMessage;
