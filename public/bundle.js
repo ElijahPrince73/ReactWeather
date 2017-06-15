97cd239d33576e22ab4e3a67a756ea58177380eb
Elijah Prince <elijahprince73@gmail.com>
2017-06-14 22:35:55 -0700

    Fixed error modal bug

--- public/bundle.js
+++ public/bundle.js
@@ -25785,6 +25785,8 @@
 	/* WEBPACK VAR INJECTION */(function($) {'use strict';
 
 	var React = __webpack_require__(8);
+	var ReactDOM = __webpack_require__(165);
+	var ReactDOMServer = __webpack_require__(261);
 
 	var ErrorModal = React.createClass({
 	  displayName: 'ErrorModal',
@@ -25799,16 +25801,11 @@
 	    message: React.PropTypes.string.isRequired
 	  },
 	  componentDidMount: function componentDidMount() {
-	    var modal = new Foundation.Reveal($('#error-modal'));
-	    modal.open();
-	  },
-	  render: function render() {
 	    var _props = this.props,
 	        title = _props.title,
 	        message = _props.message;
 
-
-	    return React.createElement(
+	    var modalMarkup = React.createElement(
 	      'div',
 	      { id: 'error-modal', className: 'reveal tiny text-center', 'data-reveal': '' },
 	      React.createElement(
@@ -25831,6 +25828,16 @@
 	        )
 	      )
 	    );
+
+	    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+
+	    $(ReactDOM.findDOMNode(this)).html($modal);
+
+	    var modal = new Foundation.Reveal($('#error-modal'));
+	    modal.open();
+	  },
+	  render: function render() {
+	    return React.createElement('div', null);
 	  }
 	});
 
@@ -27469,6 +27476,15 @@
 	exports.push([module.id, ".page-title {\n  margin-top: 10%;\n  margin-bottom: 10%;\n  color: black; }\n\n.input[type=search] {\n  box-shadow: none; }\n", ""]);
 
 	// exports
+
+
+/***/ }),
+/* 261 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	module.exports = __webpack_require__(155);
 
 
 /***/ })
